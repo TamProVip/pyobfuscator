79a8bf757638540297304428949819c4617985cc
TamPro <phantam251109@gmail.com>
2026-06-11 14:34:59 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Cài Python 3.12
+apt-get update
+apt-get install -y python3.12 python3.12-venv python3.12-dev
+
+# Cài Python 3.11
+apt-get install -y python3.11 python3.11-venv python3.11-dev
+
+# Cài Python 3.10
+apt-get install -y python3.10 python3.10-venv python3.10-dev
+apt-get install -y python3.9 python3.9-venv python3.9-dev
